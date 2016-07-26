0ae4bf0ac2235105b3d5e1b528a53b3ff176b1e5
Peter Miklos <pt.miklos@gmail.com>
2016-07-26 13:20:23 +0100

    Add separate var to show the total number of students in directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11 #separate var to store the total number of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-#we print out the total number of students manually
-print "Overall, we have "
-print 11
-puts " great students"
+#we print out the total number of students by using a var
+puts "Overall, we have #{student_count} great students"
