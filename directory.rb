2cdcf0efd10319e72ffd6654ef7146ec63a222ad
Peter Miklos <pt.miklos@gmail.com>
2016-07-26 13:25:19 +0100

    Use .each method to print out the content of students array

--- directory.rb
+++ directory.rb
@@ -5,16 +5,6 @@ students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
 #print out the list of students
 puts "The students of Villains Academy"
 puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each { |student| puts student }
 #we print out the total number of students by using a var
 puts "Overall, we have #{students.count} great students"
