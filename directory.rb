8b370c00fd0997e4266465429b32fa8e09c4f1b7
Peter Miklos <pt.miklos@gmail.com>
2016-07-26 13:45:52 +0100

    Store additional data in students array and print them out

--- directory.rb
+++ directory.rb
@@ -1,16 +1,17 @@
-students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
-  "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",
-  "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon",
-  "Norman Bates"]
-#print out the list of students
+students = [["Dr. Hannibal Lecter", :november], ["Darth Vader", :november],
+  ["Nurse Ratched", :november], ["Michael Corleone", :november],
+  ["Alex DeLarge", :november], ["The Wicked Witch of the West", :november],
+  ["Terminator", :november], ["Freddy Krueger", :november], ["The Joker", :november],
+  ["Joffrey Baratheon", :november], ["Norman Bates", :november]]
+
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(names)
-  names.each { |name| puts name }
+def print(students)
+  students.each { |student| puts "#{student[0]} (#{student[1]} cohort)" }
 end
 
 def print_footer(names)
