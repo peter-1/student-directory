64d31e123e2ead89cfa2520f20e3931b5eadf527
Peter Miklos <pt.miklos@gmail.com>
2016-07-27 18:38:42 +0100

    Update directory.rb to be able to take arguments from the command line

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp) #get selection from user and pass it to process method
+    process(STDIN.gets.chomp) #get selection from user and pass it to process method
   end
 end
 
@@ -35,14 +35,14 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp #get the first name
+  name = STDIN.gets.chomp #get the first name
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -70,8 +70,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -79,4 +79,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.size} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quit the program
+  end
+end
+
+try_load_students
 interactive_menu
