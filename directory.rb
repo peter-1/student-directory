f54a909b9f46b142afd41180b8048a11d3733b6b
Peter Miklos <pt.miklos@gmail.com>
2016-07-27 18:19:11 +0100

    Refactoring code in directory.rb

--- directory.rb
+++ directory.rb
@@ -1,38 +1,45 @@
+@students = [] #create an empty array
+
 def interactive_menu
-  students = [] #create an empty array
   loop do
-    #print out the menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp #get selection from user
-    case selection
-    when "1" then students = input_students
-    when "2"
-      print_header; print(students); print_footer(students)
-    when "9" then exit #exists the program
-    else
-      puts "I don't know what you meant, please try again."
-    end
+    print_menu
+    process(gets.chomp) #get selection from user and pass it to process method
   end
+end
+
+def print_menu
+  #print out the menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1" then input_students
+  when "2" then show_students
+  when "9" then exit #exists the program
+  else
+    puts "I don't know what you meant, please try again."
+  end
+end
+
+def show_students
+  print_header; print_students_list; print_footer
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = [] #create an empty array
-  #get the first name
-  name = gets.chomp
+  name = gets.chomp #get the first name
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
 
 def print_header
@@ -40,14 +47,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
